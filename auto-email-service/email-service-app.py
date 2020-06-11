f55a2f5117525485767cca4e459f4069741e5130
Rishu <rishu.shrivastava@gmail.com>
2020-06-11 22:57:35 +0100

    First Commit

---
+++ auto-email-service/email-service-app.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world(): 
+    return "Hello World ! testing out docker"
+
+
+
+if __name__ == '__main__': 
+    print("Starting AUTO EMAIL Service")
+    app.run(host='0.0.0.0', port=5000, debug=True)
